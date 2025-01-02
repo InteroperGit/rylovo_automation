d5063eb77705e04d25dfdf1c47251d3c46cbe8d8
Maxim Matveev <maxx.matveev@gmail.com>
2025-01-03 00:06:44 +0300

    [Feature] ImageLoggingService. Add image saving

--- src/services/image-logging-service/src/image_logging_service.py
+++ src/services/image-logging-service/src/image_logging_service.py
@@ -5,6 +5,7 @@ import base64
 import cv2
 import time
 import numpy as np
+from datetime import datetime
 
 SLEEP_INTERVAL = 5
 
@@ -15,8 +16,16 @@ class ImageLoggingService:
         password = os.getenv("RABBITMQ_PASSWORD")
         rabbitmq_host = os.getenv("RABBITMQ_HOST")
         exchange_name = os.getenv("RABBITMQ_EXCHNAGE_NAME")
+        root_folder = os.getenv("IMAGE_ROOT_FOLDER")
         
-        return max_attempts, user, password, rabbitmq_host, exchange_name
+        return {
+            "max_attempts": max_attempts,
+            "user": user,
+            "password": password,
+            "rabbitmq_host": rabbitmq_host,
+            "exchange_name": exchange_name,
+            "root_folder": root_folder
+        }
     
     def __try_to_connect(self, max_attempts, rabbitmq_host, credentials):
         attempts = 0
@@ -33,17 +42,19 @@ class ImageLoggingService:
                 attempts += 1
     
     def __init_rabbitmq(self):
-        max_attempts, user, password, rabbitmq_host, exchange_name = self.__read_data()
+        data = self.__read_data()
         
         try:
-            credentials = pika.PlainCredentials(user, password)
-            self._EXCHANGE_NAME = exchange_name
+            credentials = pika.PlainCredentials(data["user"], data["password"])
+            self._EXCHANGE_NAME = data["exchange_name"]
+            self._ROOT_FOLDER = data["root_folder"]
+            
             # Инициализация подключения
-            print(f"Try to connect to RabbitMQ server: {rabbitmq_host} with credentials: {user}:{password}")
-            self.__try_to_connect(max_attempts, rabbitmq_host, credentials)
+            print(f"Try to connect to RabbitMQ server: {data["rabbitmq_host"]} with credentials: {data["user"]}:{data["password"]}")
+            self.__try_to_connect(data["max_attempts"], data["rabbitmq_host"], credentials)
                     
             if self._connection is None:
-                raise Exception(f"Failed to connect to RabbitMQ after {max_attempts} attempts.")
+                raise Exception(f"Failed to connect to RabbitMQ after {data["max_attempts"]} attempts.")
                 
             self._channel = self._connection.channel()
             self._channel.exchange_declare(exchange=self._EXCHANGE_NAME, exchange_type='fanout')
@@ -68,22 +79,60 @@ class ImageLoggingService:
         self._ready = rabbitmq_ready
         self._started = False
         
+    def __parse_body(self, body):
+        # Распаковка JSON-сообщения
+        message = json.loads(body)
+        timestamp = message.get("timestamp")
+        camera_id = message.get("camera_id")
+        encoded_image = message.get("image")
+        
+        return timestamp, camera_id, encoded_image
+    
+    def __decode_image(self, encoded_image):
+        # Декодирование изображения
+        image_data = base64.b64decode(encoded_image)
+        np_image = np.frombuffer(image_data, dtype=np.uint8)
+        return cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+           
+    def __save_image(self, image):
+        # Сохраняем изображение
+        root_folder = self._ROOT_FOLDER
+        current_datetime = datetime.now()
+        formatted_date = current_datetime.strftime("%Y-%m-%d/%H")
+        ms = f"{current_datetime.microsecond // 1000:03d}"
+        formatted_datetime = f"{current_datetime.strftime('%Y%m%d_%H%M%S')}_{ms}"
+        
+        # Формируем путь к папке
+        save_path = os.path.join(root_folder, formatted_date)
+        
+        # Создаём директории, если они ещё не существуют
+        os.makedirs(save_path, exist_ok=True)
+        
+        # Формируем имя файла по шаблону
+        file_name = f"image_{formatted_datetime}.jpg"
+        full_path = os.path.join(save_path, file_name)
+        
+        success = cv2.imwrite(full_path, image)
+        
+        # Сохраняем изображение
+        return success, full_path
+           
     def __callback(self, ch, method, properties, body):
         try:
-            # Распаковка JSON-сообщения
-            message = json.loads(body)
-            timestamp = message.get("timestamp")
-            camera_id = message.get("camera_id")
-            encoded_image = message.get("image")
-            
-            # Декодирование изображения
-            image_data = base64.b64decode(encoded_image)
-            np_image = np.frombuffer(image_data, dtype=np.uint8)
-            image = cv2.imdecode(np_image, cv2.IMREAD_COLOR)
+            timestamp, camera_id, encoded_image = self.__parse_body(body)
+            image = self.__decode_image(encoded_image)
 
             # Вывод информации
             print(f"Получено изображение от камеры {camera_id} с timestamp: {timestamp}")
             print(f"Размер изображения: {image.shape}")
+            
+            save_successed, full_path = self.__save_image(image)
+            
+            if save_successed:
+                print(f"Изображение успешно сохранено по пути: {full_path}")
+            else:
+                print(f"Ошибка при сохранении изображения.")
+            
         except Exception as e:
             print(f"Ошибка обработки сообщения: {e}")
             # Не подтверждаем сообщение, чтобы оно осталось в очереди
