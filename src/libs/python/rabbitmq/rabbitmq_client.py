d564f1aacb78069a358117eb5aca9ba418f248e1
Maxim Matveev <maxx.matveev@gmail.com>
2025-01-08 20:15:49 +0300

    [Feature] Improved rabbitmq_client
    Доработан RabbitMQ Client
    
    - connect вынесен в отдельный метод
    - start/stop переименованы start_consuming/stop_consuming
    - добавлен метод send

--- src/libs/python/rabbitmq/rabbitmq_client.py
+++ src/libs/python/rabbitmq/rabbitmq_client.py
@@ -7,6 +7,7 @@ SLEEP_INTERVAL = 5
 class RabbitMQClient:
     def __init__(self, configuration: Dict[str, str]) -> None:
         self.__configuration = configuration
+        self.__connected = False
         self.__started = False
         
     def __try_to_connect(self):
@@ -30,7 +31,21 @@ class RabbitMQClient:
                 time.sleep(SLEEP_INTERVAL)  # Задержка перед повторной попыткой
                 attempts += 1
         
-    def __connect(self, callback: Callable[[], None]):
+    def __init_consuming(self, callback: Callable[[], None]) -> None:
+        config = self.__configuration
+        rabbitmq_exchange_name = config.get("rabbitmq_exchange_name")
+        
+        # Создаем уникальную очередь для каждого получателя
+        result = self.__channel.queue_declare(queue='', exclusive=True)
+        self.__queue_name = result.method.queue
+        
+        # Привязываем очередь к обменнику
+        self.__channel.queue_bind(exchange=rabbitmq_exchange_name, queue= self.__queue_name)
+        self.__channel.basic_consume(queue= self.__queue_name, on_message_callback=callback, auto_ack=True)
+        
+        print(f"RabbitMQ. Open queue: [{ self.__queue_name}]")
+      
+    def connect(self):
         config = self.__configuration
         
         connection_max_attempts = config.get("rabbitmq_connection_max_attempts")
@@ -50,30 +65,21 @@ class RabbitMQClient:
         self.__channel.exchange_declare(exchange=rabbitmq_exchange_name, exchange_type='fanout')
         print(f"RabbitMQ. Open exchange: [{rabbitmq_exchange_name}]")
         
-        # Создаем уникальную очередь для каждого получателя
-        result = self.__channel.queue_declare(queue='', exclusive=True)
-        queue_name = result.method.queue
+        self.__connected = True
         
-        # Привязываем очередь к обменнику
-        self.__channel.queue_bind(exchange=rabbitmq_exchange_name, queue=queue_name)
-        self.__channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-        print(f"RabbitMQ. Open queue: [{queue_name}]")
+    def start_consuming(self, callback: Callable[[], None]) -> None:
+        if not self.__connected:
+            return
         
-        return True
-        
-    def start(self, callback: Callable[[], None]) -> None:
         if self.__started:
             return
         
-        connected = self.__connect(callback)
-        
-        if connected:
-            self.__channel.start_consuming()
-            
+        self.__init_consuming(callback)
+        self.__channel.start_consuming()
         self.__started = True
             
-    def stop(self) -> None:
-        if not self.__started or self._connection is None:
+    def stop_consuming(self) -> None:
+        if not self.__started or not self.__connected:
             return
         
         try:
@@ -81,3 +87,16 @@ class RabbitMQClient:
             self.__connection.close()
         finally:
             self.__started = False
+            
+    def send(self, queue_name, message) -> None:
+        if self.__channel is None:
+            return
+        
+        self.__channel.basic_publish(
+            exchange=queue_name,
+            routing_key="",
+            body=message,
+            properties=pika.BasicProperties(
+                delivery_mode=2  # Сохранение сообщения на диск
+            )
+        )
