b61d0306cac47d2c1265366179eaf21a5201103d
Maxim Matveev <maxx.matveev@gmail.com>
2025-01-11 17:50:41 +0300

    [Refactoring] ImageLoggingService

--- src/services/image-logging-service/src/main.py
+++ src/services/image-logging-service/src/main.py
@@ -12,16 +12,16 @@ if __name__ == "__main__":
     try:
         container = ApplicationContainer()
         service = container.image_logging_service()
+        service.start()
+        
+        # Настроим обработку сигнала SIGINT (например, Ctrl+C) для корректного завершения работы
+        signal.signal(signal.SIGINT, signal_handler)
+
+        # Ожидаем сигналов ОС, служба будет работать, пока не получит сигнал
+        print("Служба работает. Нажмите Ctrl+C для выхода.")
+
+        while True:
+            time.sleep(1)  # Спим 1 секунду, не давая CPU простаивать, и ждем сигналов
     except Exception as e:
         print(f"Image logging service. Error: {e}")
         exit(1)
-        
-    service.start()
-    # Настроим обработку сигнала SIGINT (например, Ctrl+C) для корректного завершения работы
-    signal.signal(signal.SIGINT, signal_handler)
-
-    # Ожидаем сигналов ОС, служба будет работать, пока не получит сигнал
-    print("Служба работает. Нажмите Ctrl+C для выхода.")
-
-    while True:
-        time.sleep(1)  # Спим 1 секунду, не давая CPU простаивать, и ждем сигналов
